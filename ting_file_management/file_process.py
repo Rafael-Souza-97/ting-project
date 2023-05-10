4279e051c6892166ad81688b92502ebedb5e169d
Rafael Souza <souza_rafael.97@outlook.com>
2023-05-10 18:52:42 -0300

    feat: req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,34 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if path_file in instance.queue:
+        return None
+    txt = txt_importer(path_file)
+    instance.enqueue(path_file)
+    print(str({
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt),
+        "linhas_do_arquivo": txt}))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance.queue):
+        return sys.stdout.write("Não há elementos\n")
+    dequeued = instance.dequeue()
+    print("Arquivo", dequeued, "removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        path_file = instance.search(position)
+        txt = txt_importer(path_file)
+        metadata = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt),
+            "linhas_do_arquivo": txt
+        }
+        print(str(metadata))
+    except IndexError:
+        return print("Posição inválida", file=sys.stderr)
